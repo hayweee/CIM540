66487b5f0cc0b312cb392698d7e851c4f22bef09
hayweee <hayley.buchholz@hayleys-mbp.hgbcreative.com>
2017-01-26 18:39:00 -0500

    week 2

--- week 2/week 2 assignment/sketch.js
+++ week 2/week 2 assignment/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-
+    createCanvas(600,400);
+    background(255,0,0);
+    background(125);
+    
+    console.log("width: " + width + " " + "height: " + height);
 }
 
 function draw() {
-  
+  // x, y, width, height
+ellipse(70,70,100,100);
+    
+ellipse(250,250,50,50);
 }
--- week 2/week 2 notes/sketch.js
+++ week 2/week 2 notes/sketch.js
@@ -1,9 +1,14 @@
 var num = 100; 
+var num2 = 50;
 
 function setup() {
 console.log(num);
+console.log("num + num2 " + (num + num2));
+console.log(num * num2);
+console.log(num/num2);
+    
 }
 
 function draw() {
-  
+console.log(num);
 }

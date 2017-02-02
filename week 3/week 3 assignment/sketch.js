e2348dce6ac62cd95ae66d32e2844343d2f67385
hayweee <hayley.buchholz@Hayleys-MacBook-Pro.local>
2017-02-02 14:06:13 -0500

    week 3 notes

---
+++ week 3/week 3 assignment/sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+    createCanvas(500,500);
+    background(255,255,153);
+    
+    console.log("width: " + width + " " + "height: " + height);
+}
+
+function draw() {
+
+fill(255,255,255);
+ellipse(180,150,175,175);
+    fill(102,178,255);
+    ellipse(200,130,100,100);
+        fill(0,0,0);
+        ellipse(210,130,70,70);
+            fill(255,255,255);
+            ellipse(200,120,25,25);
+            ellipse(220,110,15,15);
+            ellipse(225,125,10,10)
+fill(255,255,255);
+ellipse(330,150,175,175);
+   fill(102,178,255);
+    ellipse(350,130,100,100);
+        fill(0,0,0);
+        ellipse(360,130,70,70);
+            fill(255,255,255);
+            ellipse(350,120,25,25);
+            ellipse(370,110,15,15);
+            ellipse(375,125,10,10);
+
+
+
+}
+
+function mousePressed(){
+    background('blue');
+}

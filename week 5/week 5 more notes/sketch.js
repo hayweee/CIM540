7ed37b88db75014f9471bdf721bf9d278d20778b
hayweee <hayley.buchholz@Hayleys-MacBook-Pro.local>
2017-02-16 13:59:28 -0500

    week5

---
+++ week 5/week 5 more notes/sketch.js
@@ -0,0 +1,23 @@
+var randomHello = [];
+
+
+function setup() {
+    console.log(randomHello.lenght);
+    console.log(typeof(randomHello));
+    
+    randomHello.push("Hello");
+    console.log(randomHello);
+    console.log(randomHello[0]);
+    
+    randomHello.push("Hola");
+    console.log(randomHello);
+    console.log(randomHello[1]);
+    
+    randomHello.push("Bonjour");
+    console.log(randomHello);
+    console.log(randomHello[2]);
+}
+
+function draw(){
+    
+}

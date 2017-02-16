e4e0f0233311343577fedaad5b68de5de9b9331a
hayweee <hayley.buchholz@Hayleys-MacBook-Pro.local>
2017-02-16 13:06:35 -0500

    week5

--- week 5/week 5 assignments/sketch.js
+++ week 5/week 5 assignments/sketch.js
@@ -1,37 +1,35 @@
-function setup() {
-    createCanvas(500,500);
-    background(255,255,153);
-    
-    console.log("width: " + width + " " + "height: " + height);
+var message = "hello";
+
+function setup(){   
 }
 
-function draw() {
-
-fill(255,255,255);
-ellipse(180,150,175,175);
-    fill(102,178,255);
-    ellipse(200,130,100,100);
-        fill(0,0,0);
-        ellipse(210,130,70,70);
-            fill(255,255,255);
-            ellipse(200,120,25,25);
-            ellipse(220,110,15,15);
-            ellipse(225,125,10,10)
-fill(255,255,255);
-ellipse(330,150,175,175);
-   fill(102,178,255);
-    ellipse(350,130,100,100);
-        fill(0,0,0);
-        ellipse(360,130,70,70);
-            fill(255,255,255);
-            ellipse(350,120,25,25);
-            ellipse(370,110,15,15);
-            ellipse(375,125,10,10);
-
-
-
+function draw(){
+    bacground(255);
+    //fill(0);
+    textSize(32);
+    
+    text(message, 10, 10, 100, 100); 
 }
 
 function mousePressed(){
-    background('blue');
+    message = "goodbye";
+}
+
+function mouseReleased(){
+    message = "";
+}
+
+
+function keyPressed(){
+    console.log(key);
+    console.log(keyCode);
+    
+        if(key == 'A'){
+            console.log("A is for Apple")
+        }
+        
+        if(keyCode == 65){
+            console.log("A is for 65")
+        }
+    
 }
